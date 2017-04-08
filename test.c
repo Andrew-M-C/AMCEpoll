0b76d0f6326dc3b6e8db770d6e5c70cf04f33300
Andrew Chang <laplacezhang@126.com>
2017-04-08 22:46:09 +0800

    [ADD] Add test.c. [ADD] 添加 test.c 文件

---
+++ test.c
@@ -0,0 +1,33 @@
+/*******************************************************************************
+	Copyright (C) 2017 by Andrew Chang <laplacezhang@126.com>
+	Licensed under the LGPL v2.1, see the file COPYING in base directory.
+	
+	File name: 	test.c
+	
+	Description: 	
+	    This is the test source for AMCEpoll. If you want to use AMCEpoll itself
+	only, please link .a or .so file.
+			
+	History:
+		2014-04-08: File created as "test.c"
+
+	------------------------------------------------------------------------
+
+	    This library is free software; you can redistribute it and/or modify it 
+	under the terms of the GNU Lesser General Public License as published by the 
+	Free Software Foundation, version 2.1 of the License. 
+	    This library is distributed in the hope that it will be useful, but WITHOUT
+	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+	FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+	details. 
+	    You should have received a copy of the GNU Lesser General Public License 
+	along with this library; if not, see <http://www.gnu.org/licenses/>.
+		
+********************************************************************************/
+
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
